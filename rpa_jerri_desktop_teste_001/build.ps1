b67afdb7321c49612c287a0204bb8a2fbdacc81b
Jerri Mattoso <jerrimattoso@gmail.com>
2025-09-20 22:24:06 -0300

    Remover arquivos obsoletos 3

--- rpa_jerri_desktop_teste_001/build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "rpa_jerri_desktop_teste_001.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "rpa_jerri_desktop_teste_001.zip" -Force
